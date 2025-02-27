b6324bbff967242ada6d574840379b8c647441dc
Rob McGuinness <codeacious.sys@gmail.com>
2025-02-27 23:42:25 +0000

    Fix compiler error

--- quiche/src/lib.rs
+++ quiche/src/lib.rs
@@ -5158,14 +5158,15 @@ impl Connection {
             Shutdown::Read => {
                 let unread_bytes = stream.recv.max_off() - stream.recv.off_front();
                 self.flow_control.add_consumed(unread_bytes);
-                if self.should_update_max_data() {
-                    self.almost_full = true;
-                }
 
                 stream.recv.shutdown()?;
 
                 if !stream.recv.is_fin() {
                     self.streams.insert_stopped(stream_id, err);
+                }
+
+                if self.should_update_max_data() {
+                    self.almost_full = true;
                 }
 
                 // Once shutdown, the stream is guaranteed to be non-readable.

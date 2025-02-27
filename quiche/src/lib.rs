ffc69f00cbca3b0b5f106ce928730b28e2b3abce
Sunjay Cauligi <scauligi@gmail.com>
2025-02-27 12:04:04 -0500

    bugfix: consume unread bytes on shutdown

--- quiche/src/lib.rs
+++ quiche/src/lib.rs
@@ -5108,6 +5108,8 @@ impl Connection {
 
         match direction {
             Shutdown::Read => {
+                let unread_bytes = stream.recv.max_off() - stream.recv.off_front();
+                self.flow_control.add_consumed(unread_bytes);
                 stream.recv.shutdown()?;
 
                 if !stream.recv.is_fin() {
